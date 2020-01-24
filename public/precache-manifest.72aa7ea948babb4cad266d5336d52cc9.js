181dd0421370ada2d76a2a919efa6ddd213b5b4c
eeeeman <eeeeman@icloud.com>
2020-01-23 20:05:48 -0500

    first commit

---
+++ public/precache-manifest.72aa7ea948babb4cad266d5336d52cc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66cc6235306eedf53faae1f415eb3088",
+    "url": "/index.html"
+  },
+  {
+    "revision": "679cf47d8ef47d72a732",
+    "url": "/static/css/main.d2afbf00.chunk.css"
+  },
+  {
+    "revision": "a24d7ff2d40ea7450b62",
+    "url": "/static/js/2.7e1e6980.chunk.js"
+  },
+  {
+    "revision": "679cf47d8ef47d72a732",
+    "url": "/static/js/main.0ef9038a.chunk.js"
+  },
+  {
+    "revision": "3faccafb6964bc7176f3",
+    "url": "/static/js/runtime-main.ee2f9a92.js"
+  }
+]);
